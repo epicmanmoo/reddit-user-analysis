b9675f09d9f8347d22a7d1a739327e0f4ff24190
Mooaz Nasir <mooaz09@gmail.com>
2020-05-09 00:48:17 -0700

    getting controversial seemed like a bad idea, doing top comment now.

---
+++ src/getTopComment.py
@@ -0,0 +1,46 @@
+from src.getbestsubreddit import *
+
+bs = BestSub()
+r = bs.red_inst()
+conn = bs.get_conn()
+cursor = bs.get_cursor()
+command = (
+    """
+        SELECT post_id from post;
+    """
+)
+cursor.execute(command)
+post_id = cursor.fetchone()[0]
+post = r.submission(id=post_id)
+comment_id = ""
+comment_author_id = ""
+comment_body = ""
+post_subreddit_id = post.subreddit.id
+subreddit_name = str(list(r.info(['t5_' + post_subreddit_id]))[0])
+comment_author = ""
+author_comment_karma = 0
+author_link_karma = 0
+
+post.comment_sort = 'top'
+for comment in post.comments:
+    if comment.controversiality == 0:
+        if comment.body != "[removed]" and comment.score > 200:
+            comment_id = comment.id
+            comment_author_id = comment.author.id
+            comment_body = comment.body
+            comment_author = comment.author
+            author_comment_karma = comment.author.comment_karma
+            author_link_karma = comment.author.link_karma
+            break
+
+command = (
+    """
+        INSERT INTO reddit_user VALUES(%s, %s, %s, %s);
+    """
+)
+to_insert = (comment_author_id, str(comment_author), author_comment_karma, author_link_karma)
+cursor.execute(command, to_insert)
+conn.commit()
+
+conn.close()
+cursor.close()
